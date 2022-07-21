046814f8b187588c0d14aa5b45190c79e4947cbb
AgungPri1 <agunggenresis@gmail.com>
2022-07-21 15:40:49 +0700

    new

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import MainPages from '../Components/MainPages'
-
+import Footer from '../Components/Footer'
 export default function MainPagesIndex() {
   return (
-    <MainPages />
+    <>
+      <div>
+        <MainPages />
+        <Footer />
+      </div>
+    </>
   )
 }
