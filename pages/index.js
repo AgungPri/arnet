64641c65485d85e89d8261ac689a5999f3bea6f0
AgungPri1 <agunggenresis@gmail.com>
2022-07-21 14:26:45 +0700

    New

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import MainPages from '../Components/MainPages'
+
+export default function MainPagesIndex() {
+  return (
+    <MainPages />
+  )
+}
